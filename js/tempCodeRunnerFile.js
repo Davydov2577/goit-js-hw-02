3efb6a0ec9eccb53daa147600ac14f537c24a73d
Aleksandr Davydov <113263675+Davydov2577@users.noreply.github.com>
2023-11-27 15:56:20 +0200

    task-1

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+function findNumber(start, end, divisor) {
+//   const x = start;
+//   const y = end;
+//   const z = divisor;
+//   let number;
+
+//   for (let i = x; i <= y; i++) {
+//     if (i % z === 0) {
+//       number = i;
+//       console.log(number);
+//     }
+//   }
+// }
